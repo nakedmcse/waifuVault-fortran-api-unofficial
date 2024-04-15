d9fee9b511fd58d3548163a8324e8d45a0844162
Walker Aldridge <daldridge@lairdscomputer.com>
2024-04-15 08:41:54 -0500

    Revert to individual o fies

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ gfortran -c httpcallbackModule.f90
 gfortran -c waifuModelsModule.f90
 gfortran -cpp -c waifuAPIModule.f90
 ar rcs lib-waifuvault.a httpcallbackModule.o waifuModelsModule.o waifuAPIModule.o
-gfortran -o waifutest -fPIC lib-waifuvault.a libcurl/libfortran-curl.a waifutest.f90 -lcurl
+gfortran -o waifutest httpcallbackModule.o waifuModelsModule.o waifuAPIModule.o libcurl/libfortran-curl.a waifutest.f90 -lcurl
